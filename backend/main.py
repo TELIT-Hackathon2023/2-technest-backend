6a2136bd8e9aec61039e6ad456e46dd1f58e809d
Atrem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2023-11-26 06:03:16 +0100

    prompt response-request

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from ChatbotService import ChatbotService
 from model import SendDataDto
-from dotenv import load_dotenv
-
-load_dotenv()
+# from dotenv import load_dotenv
+#
+# load_dotenv()
 
 app = FastAPI()
-chatbot_service = ChatbotService()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# chatbot_service = ChatbotService()
 
 
 @app.get("/prompt")
 def prompt(data: str):
-    return {"answer": chatbot_service.get_answer(data)}
+    # return {"answer": chatbot_service.get_answer(data)}
+    return  {"answer": "Test Answer, hello gays"}
 
 
 @app.post("/api/v1/hello")
