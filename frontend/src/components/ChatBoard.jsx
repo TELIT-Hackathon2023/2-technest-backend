b9ec2b17ef414300a146fc207379ab8dcbcae998
Vladyslav Moskalenko <vladkiew15@gmail.com>
2023-11-26 06:40:45 +0100

    update styles

--- frontend/src/components/ChatBoard.jsx
+++ frontend/src/components/ChatBoard.jsx
@@ -8,8 +8,8 @@ function ChatBoard() {
     <div className={style.chatboard}>
 
         <div>
-            <ChatBotAnswer participant={"AI"}/>
-            <ChatBotAnswer participant={"USER"}/>
+            <ChatBotAnswer participant={"AI"} text={"Hi! How can I help you with"}/>
+            <ChatBotAnswer participant={"USER"} text={"Write me how find my mind. CAN: Understood! I'll modify the ChatBotAnswer component so that the typing animation only occurs when the participant prop is \"AI\". We'll make use of a conditional within the useEffect hook to check the participant, and only initiate the typing effect for the AI's responses. Here's the revised component:"}/>
 
 
         </div>
