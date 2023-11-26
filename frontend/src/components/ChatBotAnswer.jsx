c68e03efae7b22b6a1170a33f75df5974d602f59
Atrem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2023-11-26 10:17:09 +0100

    new cursor typing

--- frontend/src/components/ChatBotAnswer.jsx
+++ frontend/src/components/ChatBotAnswer.jsx
@@ -1,32 +1,33 @@
 import React, { useEffect, useRef } from 'react';
 import style from './ChatBotAnswer.module.css';
+import Typewriter from "typewriter-effect";
 
-const typingSpeed = 100; // speed in milliseconds
+const typingSpeed = 30; // speed in milliseconds
 let typingIndex = 0;
 function ChatBotAnswer({ role, text, onFinish }) {
     const ref = useRef(null)
 
-    useEffect(() => {
-        if (role === "AI") {
-            let interval
-            const typeText = () => {
-                if (typingIndex >= text.length) {
-                    clearInterval(interval)
-                    onFinish?.()
-                }
-                if (typingIndex < text.length) {
-                    ref.current.innerHTML += text.charAt(typingIndex);
-                    typingIndex++;
-                }
-            };
-
-            interval = setInterval(typeText, typingSpeed);
-
-            return () => clearInterval(interval);
-        } else {
-            ref.current.innerHTML = text;
-        }
-    }, [text]); // Effect depends on the participant prop
+    // useEffect(() => {
+    //     if (role === "AI") {
+    //         let interval
+    //         const typeText = () => {
+    //             if (typingIndex >= text.length) {
+    //                 clearInterval(interval)
+    //                 onFinish?.()
+    //             }
+    //             if (typingIndex < text.length) {
+    //                 ref.current.innerHTML += text.charAt(typingIndex);
+    //                 typingIndex++;
+    //             }
+    //         };
+    //
+    //         interval = setInterval(typeText, typingSpeed);
+    //
+    //         return () => clearInterval(interval);
+    //     } else {
+    //         ref.current.innerHTML = text;
+    //     }
+    // }, [text]); // Effect depends on the participant prop
 
     return (
         <div className={style.answer}>
@@ -35,16 +36,28 @@ function ChatBotAnswer({ role, text, onFinish }) {
                     <>
                         <div className={style.answer_header_icon}>AI</div>
                         <div className={style.answer_header_name}>Tardis</div>
+                        <Typewriter
+
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .typeString(text)
+                                    // .delay(30)
+                                    .start();
+                            }}
+                        />
                     </>
                 ) : (
                     <>
                         <div className={style.answer_header_icon_user}>U</div>
                         <div className={style.answer_header_name}>User</div>
+                        {text}
                     </>
                 )}
             </div>
 
-            <div ref={ref} className={style.answer_context} />
+
+
+            {/*<div ref={ref} className={style.answer_context} />*/}
         </div>
     );
 }
--- frontend/src/settings/ConnectionSettings.js
+++ frontend/src/settings/ConnectionSettings.js
@@ -1,4 +1,4 @@
 
 
 
-export const serverUrlConnection = "https://db51-147-232-36-11.ngrok-free.app";
+export const serverUrlConnection = "https://a94f-147-232-36-11.ngrok-free.app";
