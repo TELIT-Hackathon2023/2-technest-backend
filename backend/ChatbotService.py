cd5fb70e948ed8e73ebd2476850b812d827b7dee
Atrem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2023-11-26 05:18:36 +0100

    code refactor, conflicts resolving, styles fix

--- backend/ChatbotService.py
+++ backend/ChatbotService.py
@@ -1,10 +1,40 @@
-from embedchain import Pipeline as App
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.text_splitter import MarkdownHeaderTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.chains import ConversationalRetrievalChain
+
+headers_to_split_on = [
+    ("#", "Header 1"),
+    ("##", "Header 2"),
+    ("###", "Header 3"),
+]
+
+
+def read_file(filepath: str):
+    file = open(filepath)
+    content = file.read()
+    file.close()
+    return content
+
+
+def split_markdown(data: str):
+    text_splitter = MarkdownHeaderTextSplitter(headers_to_split_on)
+    return text_splitter.split_text(data)
 
 
 class ChatbotService:
-    def __init__(self):
-        self.app = App()
-        self.app.add("./data/combined.md", data_type='mdx')
+    def init(self):
+        texts = split_markdown(read_file("./data/combined.md"))
+        embeddings = OpenAIEmbeddings()
+        db = Chroma.from_documents(texts, embeddings)
+        retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 4})
+        self.qa = ConversationalRetrievalChain.from_llm(OpenAI(), retriever)
+        self.history = []
 
-    def getAnswer(self, prompt: str):
-        return self.app.query(prompt)
+    def get_answer(self, prompt: str):
+        result = self.qa({"question": prompt, "chat_history": self.history})
+        if result is None:
+            return None
+        self.history.append((prompt, result['answer']))
+        return result['answer']
